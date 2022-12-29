026115ca31b0706aca2bb5e07dddcafb60e78322
Ankush <lothermeyer@protonmail.com>
2022-12-29 13:57:55 +0530

    added speaker session

--- core/views.py
+++ core/views.py
@@ -9,6 +9,10 @@ def team(request):
     return render(request, 'core/team.html')
 
 
+def speakers(request):
+    return render(request, 'core/speakers.html')
+
+
 def redir(request):
     response = redirect('/home/')
     return response
