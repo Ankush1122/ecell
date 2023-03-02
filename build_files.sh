afdc017a59bff262da787d56e865d9bb44e1d430
Ankush <lothermeyer@protonmail.com>
2023-03-02 22:45:07 +0530

    updated requirements file

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
  
 echo " BUILD START"
-python3 -m pip install -r requirements.txt
+python3 -m pip install pipenv
+pip install pipenv
+pipenv shell
+pipenv install django
 python3 manage.py collectstatic --noinput --clear
 echo " BUILD END"
