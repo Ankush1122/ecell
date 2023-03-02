839258afad4fc5e99e7d36f91fc35afd9d879618
Ankush <lothermeyer@protonmail.com>
2023-03-02 23:43:58 +0530

    settings configuration

--- ecell/settings.py
+++ ecell/settings.py
@@ -109,7 +109,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'core' / 'static'
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
