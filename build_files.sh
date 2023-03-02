f1b1d0393ef756a52c00c742cb42ba3b1d180af1
Ankush <lothermeyer@protonmail.com>
2023-03-02 22:50:47 +0530

    settings configuration

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
  
 echo " BUILD START"
 python3 -m pip install pipenv
-pip install pipenv
-pipenv shell
-pipenv install django
+python3 -m pip install django
 python3 manage.py collectstatic --noinput --clear
 echo " BUILD END"
